6cb82584e155dbc6910797bcbd826f98345b5d6a
Mathin04 <shaikmattu123@gmail.com>
2025-11-13 21:07:22 +0530

    Day_2

--- Day_2/Opertors.cpp
+++ Day_2/Opertors.cpp
@@ -4,5 +4,12 @@ using namespace std;
 int main(){
     int a=10;
     cout<<a++;
+    cout<<endl;
     cout<<++a;
+    int b=11
+    if(a==b){
+        cout<<"yes";
+    }else{
+        cout<<"no"
+    }
 }
